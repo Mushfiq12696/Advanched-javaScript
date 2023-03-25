4e95d859cda7a3d8798022e54aad73acb1ec6606
Mushfiq12696 <musfiqur15-12696@diu.edu.bd>
2023-03-25 10:29:56 +0600

    null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,16 @@
+let pakhi;
+
+function add(num1, num2) {
+    console.log(num1 + num2);
+
+    return;
+}
+
+function add(num1, num2) {
+    console.log(num1 + num2);
+}
+
+const result = add(13);
+// console.log(result);
+const employee = { name: 'Kashem', id: 181, phone: 0171013 };
+console.log(employee.address);
