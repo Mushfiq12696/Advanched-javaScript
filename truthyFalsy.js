cff0368bccff1423c06a3563e56a3ccb9b129bea
Mushfiq12696 <musfiqur15-12696@diu.edu.bd>
2023-03-25 09:52:18 +0600

    Truthy and Falsy values

---
+++ truthyFalsy.js
@@ -0,0 +1,20 @@
+//falsy
+//0;
+//null;
+//"";
+//false;
+//NaN;
+//undefined;
+
+//truthy
+//'0', ' ';
+//[];
+//{}'
+
+//const age = 0
+let name = "0";
+if (age) {
+    console.log('Condition is true');
+} else {
+    console.log('condition is false');
+}
